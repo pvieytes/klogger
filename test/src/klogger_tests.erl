de11df93a0b348fc54196fc56e584242e98e0259
Pablo Vieytes <pvieytes@openshine.com>
2012-11-26 10:13:21 +0100

    add common tests suite

---
+++ test/src/klogger_tests.erl
@@ -0,0 +1,31 @@
+%%-------------------------------------------------------------------
+%% @author Pablo Vieytes <mail@pablovieytes.com>
+%% @copyright (C) 2012, Pablo Vieytes
+%%
+%%   Licensed under the Apache License, Version 2.0 (the "License");
+%%   you may not use this file except in compliance with the License.
+%%   You may obtain a copy of the License at
+%%
+%%       http://www.apache.org/licenses/LICENSE-2.0
+%%
+%%   Unless required by applicable law or agreed to in writing, software
+%%   distributed under the License is distributed on an "AS IS" BASIS,
+%%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%   See the License for the specific language governing permissions and
+%%   limitations under the License.
+%%
+%% @doc
+%
+%% @end
+%% Created : 26 Nov 2012 by Pablo Vieytes <mail@pablovieytes.com>
+%%-------------------------------------------------------------------
+-module(klogger_tests).
+-include_lib("eunit/include/eunit.hrl").
+-export([start/0]).
+
+start() ->
+    eunit:test(klogger).
+
+general_test() ->
+    ?debugMsg("General tests"),
+    ?assertMatch(true, true).
